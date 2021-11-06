ed04be7b89ec4a87579bd71794da56b78fa2e637
Koray Aksoy <burakkorayaksoy@hotmail.com>
2021-11-06 17:56:38 +0300

    prepared for springboot project

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/auth/';
+const API_URL = 'http://localhost:8081/api/auth/';
 
 class AuthService {
   login(user) {
     return axios
-      .post(API_URL + 'signin', {
+      .post(API_URL + 'login', {
         username: user.username,
         password: user.password
       })
@@ -23,7 +23,7 @@ class AuthService {
   }
 
   register(user) {
-    return axios.post(API_URL + 'signup', {
+    return axios.post(API_URL + 'register', {
       username: user.username,
       email: user.email,
       password: user.password
